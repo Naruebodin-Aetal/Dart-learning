9ec2fcfae9d9135098f6a538797d01416f7bd283
Naruebodin-Aetal <naruebodin.a@ku.th>
2025-07-07 20:51:08 +0700

    second

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,26 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
- Widget build(BuildContext context){
-  return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Text('Hello World!')
-  );
- }
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Wellcome to Flutter',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.lightGreenAccent,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage(
+              'assets/images/dog2.jpg',
+            ),
+          ),
+        ),
+        backgroundColor: Colors.black,
+      ),
+    );
+  }
 }
